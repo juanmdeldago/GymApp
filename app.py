5c3971b7dc99955201e7b891fe48eadf99aff6e0
juanmdeldago <juanmdeldago@gmail.com>
2026-09-18 11:26:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola! El sistema del gimnasio está en línea."
+    # Esto le dice al sistema que muestre el archivo HTML que creamos
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Acá capturamos lo que el usuario escribe en la pantalla
+    usuario = request.form.get('usuario')
+    password = request.form.get('password')
+    
+    # Validamos el ingreso
+    if usuario == "admin" and password == "1234":
+        return f"¡Bienvenido {usuario}! Pronto acá veremos el panel de turnos."
+    else:
+        return "Usuario o contraseña incorrectos. Volvé atrás e intentá de nuevo."
 
 if __name__ == '__main__':
     app.run(debug=True)
